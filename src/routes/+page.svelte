8f40491798a0c9290c5652a4e6f2abe097369270
KolyStudio <97678364+KolyStudio@users.noreply.github.com>
2023-04-25 23:56:31 +0800

    first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
+<script>
+	import pseudos from './sitemap-2.xml/pseudos';
+</script>
 
 <svelte:head>
 	<title>Ma Page privée - Page d'Accueil</title>
@@ -16,10 +19,10 @@
 </svelte:head>
 
 <div class="relative py-16 bg-gradient-to-br from-sky-50 to-gray-200">
-	<div class="relative container m-auto px-6 text-gray-500 md:px-12 xl:px-40">
+	<div class="relative  m-auto px-6 text-gray-500 md:px-12 xl:px-40">
 		<div class="m-auto md:w-8/12 lg:w-6/12 xl:w-6/12">
 			<div class="rounded-xl bg-white shadow-xl">
-				<div class="p-6 sm:p-16">
+				<div class="p-6 ">
 					<div class="space-y-4">
 						<h2 class="mb-4 text-2xl text-cyan-900 font-bold text-center">Ma Page Privée</h2>
 					</div>
